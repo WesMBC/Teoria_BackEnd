16e6a53768246ad9dabb2f4648f9a85343b9e365
WesMBC <weslinmbc@gmail.com>
2024-04-06 13:01:05 -0600

    1.0.2

--- controllers/calculo.controller.js
+++ controllers/calculo.controller.js
@@ -1,4 +1,5 @@
 
+import { parse } from "dotenv";
 import { forEach } from "underscore";
 
 export const calculodegrafica = async (req, res) =>{
@@ -22,11 +23,41 @@ export const calcIndividual = async(req,res) =>{
             respuestasForm.push(conetnido);
         })
         
-        let calculo = (parseInt(respuestasForm[0])+parseInt(respuestasForm[1])+parseInt(respuestasForm[2]))*parseInt(respuestasForm[3])
+        /* 
+        const s = 0.1; // tasa de ahorro
+        const d = 0.1; // tasa de depreciación
+        const n = 0.2; // crecimiento de la población
+        const g = 0.05; // progreso tecnológico
+        const alpha = 0.5; // elasticidad de la producción respecto al capital 
+        */
 
+        const s     = parseFloat(respuestasForm[0]);
+        const d     = parseFloat(respuestasForm[1]);
+        const n     = parseFloat(respuestasForm[2]);
+        const g     = parseFloat(respuestasForm[3]);
+        const alpha = parseFloat(respuestasForm[4]);
+
+    
+        // Funciones del modelo de Solow
+        const y = (k) => Math.pow(k, alpha); // Output por trabajador
+        const i = (k) => s * y(k); // Inversión por trabajador
+        const dep = (k) => (n + g + d) * k; // Depreciación efectiva por trabajador
+
+        // Generar datos para el gráfico
+        const kValues = Array.from({ length: 100 }, (_, i) => i);
+        const yValues = kValues.map((k) => y(k));
+        const iValues = kValues.map((k) => i(k));
+        const depValues = kValues.map((k) => dep(k));
+
+        const respuesta = {
+            kValues:kValues,
+            yValuyes:yValues,
+            iValues:iValues,
+            depValues:depValues
+        }
         
         
-        return res.status(200).json({contenido:calculo})
+        return res.status(200).json({respuesta})
     } catch (error) {
         console.log(error)
         error.message = "Error al momento de ejecucion del caluclo"
